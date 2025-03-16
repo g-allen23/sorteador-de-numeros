1da1fe09afdf93a92fe456a45041fc067409179a
g-allen23 <geovaneallen8@gmail.com>
2025-03-16 20:04:18 -0300

    Adição de uma função de alert, e alteração em algumas variáveis!

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
+const button = document.querySelector('button')
+const paragraph = document.querySelector('.paragraph-result')
+
+
 function cliquei() {
-    const button = document.querySelector('button')
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    const paragraph = document.querySelector('.paragraph-result')
-
     paragraph.innerHTML = `O resultado entre ${min} e ${max} é ${result}!`
     
 }
+
+function alerta(){
+    if(document.querySelector('.input-min').value >= document.querySelector('.input-max').value){
+
+        alert(`O valor do primeiro campo não pode ser maior que o valor do segundo campo!`)
+
+        paragraph.innerHTML = ``
+    }
+}
+
+button.addEventListener('click', alerta)
